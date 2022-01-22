9a86fea72c9a589f874097c647083f508813bf9c
Alex Orlet <AlexOrlet89@gmail.com>
2022-01-21 18:05:19 -0800

    added list element to html, added loadPokedex() and fixed import

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 // import functions
-import { getPokedex } from "./fetch";
+import { getPokedex } from './fetch.js';
 // grab DOM elements
+// console.log('app.js is working');
+
 const template = document.querySelector('#template');
 // const selectEl = document.querySelector('select');
 // not doing the select funciton.
 console.log('dom', template);
 
+async function loadPokedex() {
+    const pokedex = await getPokedex();
+    list.classList.add('pokemon');
+
+    for (let pokemon of pokedex) {
+        const clone = template.content.cloneNode(true);
+        const name = clone.querySelector('h2');
+        const image = clone.querySelector('img');
+        const type = clone.querySelector('h6');
+    }
+    console.log(pokemon, name, image);
+    // console.log(pokedex);
+}
+
+loadPokedex();
 // set event listeners 
     // get user input
     // use user input to update state 
